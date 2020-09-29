ca0589c35013294b0a3ce5fd7b9eaf459ed7a26d
marco villanueva <villanuevamarco98@gmail.com>
2020-09-28 20:24:58 -0600

    Add project files.

---
+++ Tests/Tests_Netframework.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests_Netframework
+    {
+        [TestMethod]
+        public void Test001()
+        {
+        }
+    }
+}
