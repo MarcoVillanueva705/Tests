30ebbebb2e5552155b35c70099deaac26c346573
marco villanueva <villanuevamarco98@gmail.com>
2020-09-29 20:34:36 -0600

    Get Enumerator notes

--- Tests/Tests_Netframework.cs
+++ Tests/Tests_Netframework.cs
@@ -17,6 +17,22 @@ namespace Tests
             //List is in the generic collection, generic in
             //the sense that it can be a list of any specified type
             var numbers = new List<int> { 1,2,3,4 };
+
+            //The thing to confirm using the Assert statement is True.
+            //is whether numbers is in fact IEnumerable
+            Assert.IsTrue(numbers is IEnumerable<int>);
+
+            //first we need to get the object that makes it possible
+            //to go through all the elements.
+
+            //We ask the 'numbers' object to provide us with a new object
+            //and enumerator that can enumerate the collection
+
+            //And what is that enumerator?
+            //e the object is
+            var e = numbers.GetEnumerator();
+            //We're saying 'numbers' as a collections is enumberable
+            Assert.IsTrue(e is IEnumerator<int>);
         }
     }
 }
