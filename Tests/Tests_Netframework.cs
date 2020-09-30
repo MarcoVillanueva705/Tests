4c450115d866e9144beb274c665dd2482eb12d8b
marco villanueva <villanuevamarco98@gmail.com>
2020-09-29 20:15:39 -0600

    List notes

--- Tests/Tests_Netframework.cs
+++ Tests/Tests_Netframework.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -8,6 +9,14 @@ namespace Tests
         [TestMethod]
         public void Test001()
         {
+            //IEnumerable: the ability to go through all of the elements
+            //one at a time
+
+            //Listbehaves similar to an array, but list
+            //has a dynamic length with type interger
+            //List is in the generic collection, generic in
+            //the sense that it can be a list of any specified type
+            var numbers = new List<int> { 1,2,3,4 };
         }
     }
 }
